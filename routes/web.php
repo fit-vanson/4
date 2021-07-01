afce332292c5cdf14038999d6973dea9390c7e34
fit-vanson <fit.vanson@gmail.com>
2021-07-01 16:28:54 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -19,21 +19,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-
-
-
-
-Route::get('/googleadsense', [GoogleAdsenseController::class, 'getIndex']);
-Route::post('/googleadsense', [GoogleAdsenseController::class, 'postIndex']);
-
-Route::resource('home', 'HomeController');
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-//Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/cron', [CronController::class, 'getIndex']);
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/googleadsense', [GoogleAdsenseController::class, 'getIndex']);
+    Route::post('/googleadsense', [GoogleAdsenseController::class, 'postIndex']);
+
+    Route::resource('home', 'HomeController');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+Route::get('/cron', [CronController::class, 'getIndex']);
+
